909d9d521e97c8eeb596497451466975cf2a03d1
devgauravjatt <devgauravjatt@gmail.com>
2025-07-18 06:58:50 +0530

    feat: add default db file name

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/libsql'
 import * as schema from './schema'
 
-export const db = drizzle(process.env.DB_FILE_NAME!, {
+const dbFileName = process.env.DB_FILE_NAME || 'file:local.db'
+
+export const db = drizzle(dbFileName, {
 	schema: schema,
 })
